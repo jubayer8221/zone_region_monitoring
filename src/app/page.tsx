f25b7d23ae6c0e19a2761bf566ef321dcf504c3d
Jubayer Al Mahmud <jubayer8221@gmail.com@users.noreply.github.com>
2025-05-05 00:16:36 +0600

    table for Operation Place Tree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Table from "@/components/Zone/Table";
+import TableGrids from "@/components/TableGrids";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       {/* <h1>This is Home page.</h1> */}
       <div className="w-full">
         <Table></Table>
+        <TableGrids></TableGrids>
       </div>
     </div>
   );
